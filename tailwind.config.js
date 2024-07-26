5e78d2d4517bfede223c55be585f559328a65b60
Rashmi <5Rashmi>
2024-07-26 19:56:18 +0530

    responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        sm: '180px', // Adjust this value as needed
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
+      },
+    },
   },
   plugins: [],
 }
